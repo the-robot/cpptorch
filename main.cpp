46ab0ca020fcad40db42fabe0cdfadb20a15e1d3
the-robot <9334746+the-robot@users.noreply.github.com>
2021-01-02 12:09:52 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,5 +4,9 @@
 int main() {
     torch::Tensor tensor = torch::rand({2, 3});
     std::cout << tensor << std::endl;
+    
+    if (torch::cuda::is_available()) {
+        std::cout << "CUDA is available! Training on GPU." << std::endl;
+    }
     return 0;
 }
